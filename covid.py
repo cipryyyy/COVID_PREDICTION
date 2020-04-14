bff9d33cdb685c46a584e0c6ed6171166c5a3751
cipryyyy <63643557+cipryyyy@users.noreply.github.com>
2020-04-14 14:07:47 +0200

    Prevision

--- covid.py
+++ covid.py
@@ -2,8 +2,10 @@ import pandas as pd
 from scipy import stats
 import numpy as np
 import matplotlib.pyplot as plt
+import warnings
+warnings.filterwarnings("ignore")
 
-df=pd.read_csv("dati_covid.csv")    #I dati sono la data, giorni passati dal primo dato, i casi totali, i morti i guariti e i casi attualmente attivi
+df=pd.read_csv("dati_covid.csv")
 x=df["giorno"]
 day=int(x[-1:].values)
 colors=["red", "green", "black", "yellow"]
@@ -14,6 +16,7 @@ def regressor(x):
 
 for i in range(4):
     y=df.iloc[:, i+2]
+    last=int(y[-1:].values)
     model=np.poly1d(np.polyfit(x,y,3))
     line=np.linspace(min(x), max(x))
     
@@ -21,10 +24,17 @@ for i in range(4):
     plt.subplot(1,2,2)
     plt.scatter(x,y, color=colors[i], alpha=0.5)
     plt.plot(line, model(line), color=colors[i])
-    pred.append(model(day))
+    pred.append(model(day+1))
+    plt.axvline(day+1)
+    plt.scatter(day+1, pred[-1:], color="blue")
     
+    result=int(pred[i]-last)
     plt.subplot(12,2, 2*i+1)
     plt.axis("off")
     plt.text(0,0,f"{df.columns[i+2].capitalize()}: {int(pred[i])}")
-    print(f"{df.columns[i+2]} previsti il 14 aprile 2020: {int(pred[i])}")
+    if result>=0:
+        plt.text(0.4,0,f"[+{result}]")
+    else:
+        plt.text(0.4,0,f"[{result}]")
+    print(f"{df.columns[i+2].capitalize()} previsti il 14 aprile 2020:\t{int(pred[i])}")
 plt.show()
